eb24c9c6ef4c93e65c185a552210c8bc4006cbee
faguiarn <felipesaguiarn@gmail.com>
2021-06-29 19:47:43 -0300

    att3

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,27 @@
-import {useHistory} from 'react-router-dom'
 
-import { firebase, auth } from '../services/firebase'
+import {useHistory} from 'react-router-dom'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
-
 import { Button } from '../components/Button'
 import '../styles/auth.scss'
+import { useAuth } from '../hooks/useAuth'
+
 
 export function Home(){
     const history = useHistory();
+    const { user, signInWithGoogle } = useAuth()
+    
 
-    function handleCreateRoom(){
-        const provider = new firebase.auth.GoogleAuthProvider();
-
-        auth.signInWithPopup(provider).then(result =>{
-            console.log(result);
-            history.push('/rooms/new');
-        })
-
-        
+    async function handleCreateRoom(){
+        if(!user){
+           await signInWithGoogle()
+        }
+        history.push('/rooms/new');
+                
     }
-
-
     return(
         <div id="page-auth">
             <aside>
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,12 +1,16 @@
 import {Link} from 'react-router-dom'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 
+import { Button } from '../components/Button'
+import { useAuth } from '../hooks/useAuth'
 
 import '../styles/auth.scss'
-import { Button } from '../components/Button'
 
 export function NewRoom(){
+    const {user} = useAuth()
+
     return(
         <div id="page-auth">
             <aside>
@@ -14,12 +18,13 @@ export function NewRoom(){
                 <strong>Crie salas de Q&amp;A ao-vivo</strong>
                 <p>Tire as dúvidas da sua audiência em tempo-real</p>
             </aside>
-            <main>
+            <main>           
                 <div className="main-content" >
                     <img src={logoImg} alt="logo Letmeask" />
-                    <h2>Criar uma nova sala</h2>                    
-                    <form action="">
-                        <input type="text" 
+                    <h2>Criar uma nova sala</h2>                
+                    <form>
+                        <input 
+                        type="text" 
                         placeholder="Nome da sala"
                         />
                         <Button type="submit"> 
