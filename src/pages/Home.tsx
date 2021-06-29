80e5e7184cb7c3f5c3e1f73f64ce2446dacd8db1
faguiarn <felipesaguiarn@gmail.com>
2021-06-29 14:48:52 -0300

    att

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,27 @@
 import {useHistory} from 'react-router-dom'
 
+import { firebase, auth } from '../services/firebase'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
-import '../styles/auth.scss'
+
 import { Button } from '../components/Button'
+import '../styles/auth.scss'
 
 export function Home(){
     const history = useHistory();
 
-    function navigateToNewRoom(){
-        history.push('/rooms/new');
+    function handleCreateRoom(){
+        const provider = new firebase.auth.GoogleAuthProvider();
+
+        auth.signInWithPopup(provider).then(result =>{
+            console.log(result);
+            history.push('/rooms/new');
+        })
+
+        
     }
 
 
@@ -25,7 +35,7 @@ export function Home(){
             <main>
                 <div className="main-content" >
                     <img src={logoImg} alt="logo Letmeask" />
-                    <button onClick={navigateToNewRoom} className="create-room" >
+                    <button onClick={handleCreateRoom} className="create-room" >
                         <img src={googleIconImg} alt="Logo Google" />
                         Crie sua sala com o Google
                     </button>
